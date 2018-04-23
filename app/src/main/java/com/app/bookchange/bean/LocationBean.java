93295b9dfb244a3c2d17bb81d298dbc880d7e75a
Administrator <15708919367@163.com>
2018-04-23 10:56:32 +0800

    修改了界面结构，以及借书功能的实现

---
+++ app/src/main/java/com/app/bookchange/bean/LocationBean.java
@@ -0,0 +1,72 @@
+package com.app.bookchange.bean;
+
+import com.baidu.mapapi.model.LatLng;
+
+import java.io.Serializable;
+
+public class LocationBean implements Serializable{
+    private int id;
+    private int userid;
+    private double lng,lat;
+    private LatLng latlng;
+    private String myloctionmessage;
+
+
+    private MyBook book;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public LatLng getLatlng() {
+        return latlng;
+    }
+
+    public void setLatlng(LatLng latlng) {
+        this.latlng = latlng;
+    }
+
+    public String getMyloctionmessage() {
+        return myloctionmessage;
+    }
+
+    public void setMyloctionmessage(String myloctionmessage) {
+        this.myloctionmessage = myloctionmessage;
+    }
+
+    public MyBook getBook() {
+        return book;
+    }
+
+    public void setBook(MyBook book) {
+        this.book = book;
+    }
+}
---
+++ app/src/main/java/com/app/bookchange/view/InfoWindowHolder.java
@@ -0,0 +1,12 @@
+package com.app.bookchange.view;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class InfoWindowHolder {
+    public TextView tv_location;
+    public TextView tv_bookname;
+    public TextView tv_author;
+    public Button btn_borrowbook;
+
+}
